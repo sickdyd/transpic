b17942461394ea4564091aac221faf330e5d06da
Rob <roberto.reale.ja@gmail.com>
2021-05-15 12:11:36 +0900

    Add ts, eslint and prettier

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+export default function Home(): JSX.Element {
+  return (
+    <div>
+      <Head>
+        <title>Transpic</title>
+        <meta name="description" content="Translate text and get related image" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main></main>
+
+      <footer></footer>
+    </div>
+  )
+}
